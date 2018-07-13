073acb92e84744752608445ffed2b9d8e7d34b2b
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2018-07-13 13:43:31 +0800

    backup.

---
+++ javascript-html/src/main/java/idv/hsiehpinghan/javascripthtml/JavascriptHtmlApplication.java
@@ -0,0 +1,12 @@
+package idv.hsiehpinghan.javascripthtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavascriptHtmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavascriptHtmlApplication.class, args);
+	}
+}
---
+++ javascript-html/src/main/java/idv/hsiehpinghan/javascripthtml/controller/JavascriptController.java
@@ -0,0 +1,54 @@
+package idv.hsiehpinghan.javascripthtml.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/javascript")
+public class JavascriptController {
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index() {
+		return "javascript/index";
+	}
+
+	@RequestMapping(value = "/elementByXpath", method = RequestMethod.GET)
+	public String elementByXpath() {
+		return "javascript/elementByXpath";
+	}
+
+	@RequestMapping(value = "/xpathOfElement", method = RequestMethod.GET)
+	public String xpathOfElement() {
+		return "javascript/xpathOfElement";
+	}
+
+	@RequestMapping(value = "/for", method = RequestMethod.GET)
+	public String for_() {
+		return "javascript/for";
+	}
+
+	@RequestMapping(value = "/switch", method = RequestMethod.GET)
+	public String switch_() {
+		return "javascript/switch";
+	}
+
+	@RequestMapping(value = "/while", method = RequestMethod.GET)
+	public String while_() {
+		return "javascript/while";
+	}
+
+	@RequestMapping(value = "/regex", method = RequestMethod.GET)
+	public String regex() {
+		return "javascript/regex";
+	}
+
+	@RequestMapping(value = "/string", method = RequestMethod.GET)
+	public String string() {
+		return "javascript/string";
+	}
+
+	@RequestMapping(value = "/copyText", method = RequestMethod.GET)
+	public String copyText() {
+		return "javascript/copyText";
+	}
+}
