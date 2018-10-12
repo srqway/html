80a4dbcaf56b43aaa46ecfed22b2d65d8d8383c9
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2018-10-12 15:45:50 +0800

    backup.

---
+++ conllu-html/src/main/java/idv/hsiehpinghan/conlluhtml/ConlluHtmlApplication.java
@@ -0,0 +1,12 @@
+package idv.hsiehpinghan.conlluhtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConlluHtmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConlluHtmlApplication.class, args);
+	}
+}
